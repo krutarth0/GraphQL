0a156ca9143880ef8b24942cabf726fdd7997731
GraphQL <krutarthzero@hotmail.com>
2019-03-01 17:50:31 +0530

    added cors

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ const app = express();
 
 //allow cross-origin request
 app.use(cors());
-
 // connect to mlab database
 // make sure to replace my db string & creds with your own
 mongoose.connect('mongodb://react:react123@cluster0-shard-00-00-qlwi9.mongodb.net:27017,cluster0-shard-00-01-qlwi9.mongodb.net:27017,cluster0-shard-00-02-qlwi9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {useNewUrlParser: true})
@@ -24,5 +23,6 @@ app.use('/graphql', graphqlHTTP({
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000 !!');
+
 
 });

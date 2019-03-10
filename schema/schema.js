01d1ea8e174a9750a9c1d6da566d6a4ad0e4a772
krutarth0 <krutarthzero@hotmail.com>
2019-03-10 12:31:41 +0530

    added Author queries

--- schema/schema.js
+++ schema/schema.js
@@ -9,77 +9,81 @@ const {
     GraphQLID,
     GraphQLInt,
     GraphQLList,
+    GraphQLFloat,
     GraphQLNonNull
 } = graphql;
 
 
-const transformResponse = (items) => items.map(items=>(
+const transformBooks = (items) => items.map(items=>(
   {
-      id : items.id,
-      name : items.volumeInfo.title,
-      genre : items.volumeInfo.categories,
-      author : items.volumeInfo.authors,
-      description: items.volumeInfo.description
+    id : items.id,
+    title : items.volumeInfo.title,
+    subtitle:items.volumeInfo.subtitle,
+    genre : items.volumeInfo.categories,
+    author : items.volumeInfo.authors,
+    language:items.volumeInfo.language,
+    pageCount:items.volumeInfo.pageCount,
+    textSnippet:items.searchInfo.textSnippet,
+    description: items.volumeInfo.description,
+    maturityRating:items.volumeInfo.maturityRating,
 })
 );
 
-const transformResponseID = (items) =>
+const transformBook = (items) =>
   ({
       id : items.id,
-      name : items.volumeInfo.title,
+      title : items.volumeInfo.title,
+      subtitle:items.volumeInfo.subtitle,
       genre : items.volumeInfo.categories,
       author : items.volumeInfo.authors,
-      description: items.volumeInfo.description
+      language:items.volumeInfo.language,
+      pageCount:items.volumeInfo.pageCount,
+      textSnippet:items.searchInfo.textSnippet,
+      description: items.volumeInfo.description,
+      maturityRating:items.volumeInfo.maturityRating,
+
 
 });
 
-const transformXml = (result) =>({
-      id : result[0].$.id,
-      name : result[0].name
-});
 
-const transformXmlB = (book) => book.map(book=>//console.log(book);
-      ({
-           id : book.id[0]._,
-           name : book.title[0],
-           genre : ["NO GENRE"],
-           author : book.authors[0].author[0].name,
-           description: book.description[0]
-  })
-);
+const transformAuthor = (items) =>
+   ({
+       name : items[0].volumeInfo.authors[0],
+       genre: [...new Set( items.map(items=> items.volumeInfo.categories!==undefined ? (items.volumeInfo.categories).toString() : " ") )]
+ })
+
 const BookType = new GraphQLObjectType({
 
     name: 'Book',
     fields: ( ) => ({
         id: { type: GraphQLID },
-        name: { type: GraphQLString },
+        title: { type: GraphQLString },
+        subtitle:{ type: GraphQLString },
         genre:  {type : new GraphQLList(GraphQLString)},
         author: {type : new GraphQLList(GraphQLString)},
-        description:{type : GraphQLString }
+        language:{ type: GraphQLString },
+        pageCount:{type:GraphQLInt},
+        textSnippet:{ type: GraphQLString },
+        description:{type : GraphQLString },
+        maturityRating:{type : GraphQLString }
+
     })
 });
 
 const AuthorType = new GraphQLObjectType({
-
     name: 'Author',
     fields: ( ) => ({
-        id: { type: GraphQLID },
-        name: {type : new GraphQLList(GraphQLString)},
+        name: { type: GraphQLString },
+        genre:{type : new GraphQLList(GraphQLString)},
         books: {
           type: new GraphQLList(BookType),
           resolve(parent,args){
             return (
-            axios.get(`https://www.goodreads.com/author/list/${parent.id}?format=xml&key=MxWOOlQcCaNvPUaOHiNp3g`)
-            .then(res =>{
-                let respB
-                parseString(res.data, function (err, result) {
-                  respB=result
-                  })
-                return transformXmlB(respB.GoodreadsResponse.author[0].books[0].book);//respB.GoodreadsResponse.author[0].books[0].book[0].id[0]._
-            })
-          )
-        }
-        }
+            axios.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${parent.name}`)
+            .then(res=>transformBooks(res.data.items))
+                    )
+                  }
+              },
     })
 });
 
@@ -96,7 +100,7 @@ const RootQuery = new GraphQLObjectType({
                 return (
                   axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${args.search}&maxResults=${args.howmany}&printType=books&orderBy=relevance`)
                   // .then(res=>console.log(res.data.items.map(items=>items.volumeInfo)))
-                  .then(res =>transformResponse(res.data.items))
+                  .then(res =>transformBooks(res.data.items))
                 )
             }
         },
@@ -109,7 +113,7 @@ const RootQuery = new GraphQLObjectType({
           resolve(parent,args){
             return(
             axios.get(`https://www.googleapis.com/books/v1/volumes/${args.id}`)
-            .then(res =>transformResponseID(res.data))
+            .then(res =>transformBook(res.data))
           )
         }
       },
@@ -121,18 +125,12 @@ const RootQuery = new GraphQLObjectType({
         },
         resolve(parent,args){
           return(
-          axios.get(`https://www.goodreads.com/api/author_url/${args.name}?key=MxWOOlQcCaNvPUaOHiNp3g`)
-          .then(res =>{
-              let resp
-              parseString(res.data, function (err, result) {
-                resp=result
-                })
-              return transformXml(resp.GoodreadsResponse.author);
-          })
+          axios.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${args.name}`)
+          .then(res =>transformAuthor(res.data.items))
         )
       }
     }
-    }
+  }
 });
 
 
